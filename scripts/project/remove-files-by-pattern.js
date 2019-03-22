d1472ea4e76e81d42ff0bba15204c9b1580672d5
Nikita Sergeev <nikita.sergeev@arrival.services>
2019-03-22 14:46:08 +0300

    COREUI-276 common: added jsdoc to remove files by pattern script

--- scripts/project/remove-files-by-pattern.js
+++ scripts/project/remove-files-by-pattern.js
@@ -1,3 +1,14 @@
+/**
+ * Script removes all files that matches specified pattern in folder you also provide.
+ *
+ * Parameters:
+ *  --searchDir Directory where script will do search
+ *  @type {string}
+ *
+ *  --pattern RegExp pattern without slashes (/). For example, styles.d.ts$ becomes /styles.d.ts$/
+ *  @type {string}
+ */
+
 const { walkThrough } = require('../common/common');
 const path = require('path');
 const fs = require('fs');
