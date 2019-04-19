624dc364a00233a0893957510a5f3e9f8906f9ff
Nikita Agupov <nktrsk@gmail.com>
2019-04-19 15:39:25 +0300

    COREUI-299 common: added script to correct branch name

---
+++ scripts/ci/correct-git-branch-name.js
@@ -0,0 +1,31 @@
+const argv = require('../common/parse-process-args')(process.argv.slice(2));
+const childProcess = require('child_process');
+const exec = childProcess.execSync;
+
+const autoCalculatedBranchName = (argv.get('branch') || [])[0];
+
+if (!Boolean(autoCalculatedBranchName)) {
+    console.log('Must pass auto calculated branch name as run param with key "branch"');
+    process.exit(1);
+}
+
+const lastCommit = run('git log -1 --oneline');
+
+const mergeCommitNameMatcher = /Merge remote-tracking branch 'origin\/.+' into (.+)$/g;
+if (!mergeCommitNameMatcher.test(lastCommit)) {
+    returnResult(autoCalculatedBranchName);
+}
+
+returnResult(autoCalculatedBranchName);
+
+function run(command) {
+    console.log('run command:', command);
+    const stdout = exec(command, { encoding: 'utf8' });
+    console.log(stdout);
+    return stdout;
+}
+
+function returnResult(result) {
+    console.log(result);
+    process.exit(0);
+}
