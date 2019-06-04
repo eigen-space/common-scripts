03faffc6a7eaa17ade4edc117818fb22418dd5a7
Nikita Agupov <nktrsk@gmail.com>
2019-06-04 16:27:56 +0300

    microfix common: move detect autocommit script to build

--- dev/scripts/copy-assets.js
+++ dev/scripts/copy-assets.js
@@ -2,3 +2,5 @@ const { copy } = require('../../dist/index');
 
 const target = 'dist';
 copy(['package.json', 'yarn.lock'], target);
+
+copy(['src/scripts/ci/detect-autocommit.sh'], `${target}/scripts/ci`);
