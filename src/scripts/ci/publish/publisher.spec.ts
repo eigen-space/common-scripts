f8ccbe826af4f1c82ab1c1ed0ffd8c9b34e2905b
Nikita Sergeev <sergeev.nickitos@yandex.ru>
2020-11-16 14:46:59 +0300

    #41 ci/publish/publisher: changed puclish pipeline to sync version in package and project repository

--- src/scripts/ci/publish/publisher.spec.ts
+++ src/scripts/ci/publish/publisher.spec.ts
@@ -1,8 +1,6 @@
 import { Publisher } from './publisher';
 import * as fs from 'fs';
 import { PublishErrorType } from './enums';
-import { GitExecutor } from '../../../common/executors/git.executor';
-import { NpmExecutor } from '../../../common/executors/npm.executor';
 import Mock = jest.Mock;
 
 jest.mock('child_process');
@@ -14,17 +12,19 @@ describe('Publisher', () => {
         const PACKAGE_JSON = { name: '@eigenspace/helper-scripts', version: '2.0.10' };
         (fs.readFileSync as Mock).mockReturnValue(Buffer.from(JSON.stringify(PACKAGE_JSON)));
 
-        const MockGitExecutor = jest.fn<GitExecutor>(() => ({
-            branch: jest.fn(),
-            checkout: jest.fn(),
-            commit: jest.fn(),
-            push: jest.fn()
-        }));
-        const MockNpmExecutor = jest.fn<NpmExecutor>(() => ({
-            publish: jest.fn(),
-            view: jest.fn(),
-            search: jest.fn().mockReturnValue(JSON.stringify([{ name: PACKAGE_JSON.name }]))
-        }));
+        const MockGitExecutor = jest.fn()
+            .mockImplementation(() => ({
+                branch: jest.fn(),
+                checkout: jest.fn(),
+                commit: jest.fn(),
+                push: jest.fn()
+            }));
+        const MockNpmExecutor = jest.fn()
+            .mockImplementation(() => ({
+                publish: jest.fn(),
+                view: jest.fn(),
+                search: jest.fn().mockReturnValue(JSON.stringify([{ name: PACKAGE_JSON.name }]))
+            }));
         const gitExec = new MockGitExecutor();
         const npmExec = new MockNpmExecutor();
         const publisher = new Publisher(gitExec, npmExec);
@@ -36,6 +36,11 @@ describe('Publisher', () => {
 
             expect(gitExec.branch).toHaveBeenCalled();
             expect(gitExec.push).toHaveBeenCalledWith('master');
+        });
+
+        it('should checkout branch if branch parameter passed', () => {
+            publisher.start('master');
+            expect(gitExec.checkout).toHaveBeenCalled();
         });
 
         it('should publish several directories', () => {
@@ -51,23 +56,20 @@ describe('Publisher', () => {
             expect(npmExec.publish).not.toHaveBeenCalled();
         });
 
-        it('should checkout branch if branch parameter passed', () => {
-            publisher.start('master');
-            expect(gitExec.checkout).toHaveBeenCalled();
-        });
-
         it('should throw exception if package is already in npm repository', () => {
             (npmExec.view as Mock).mockReturnValueOnce(true);
             expect(() => publisher.start('master')).toThrow(PublishErrorType.ALREADY_IN_REGISTRY);
         });
 
-        it('should publish package and update version in package json and push with auto ci commit', () => {
+        it(`should increment version in package then publish it 
+        after that increment version in project and push it with auto ci commit`, () => {
             publisher.start('master');
 
-            expect(npmExec.publish).toHaveBeenCalled();
             const incrementedPackageJson = JSON.stringify({ ...PACKAGE_JSON, version: '2.0.11' }, null, 4);
-            expect(fs.writeFileSync).toBeCalledWith(expect.anything(), incrementedPackageJson);
+            expect(fs.writeFileSync).toHaveBeenNthCalledWith(1, expect.anything(), incrementedPackageJson);
+            expect(npmExec.publish).toHaveBeenCalled();
 
+            expect(fs.writeFileSync).toHaveBeenNthCalledWith(2, expect.anything(), incrementedPackageJson);
             const commitPattern = /auto\/ci:.*/;
             expect(gitExec.commit).toHaveBeenCalledWith(expect.stringMatching(commitPattern));
             expect(gitExec.push).toHaveBeenCalledWith('master');
